2e3e19980831934abce7951c1bac57553ede1abd
raffaele messuti <raffaele@atomotic.com>
2023-03-27 16:30:56 +0200

    NewMetadata() helper

--- v3/metadata.go
+++ v3/metadata.go
@@ -4,3 +4,10 @@ type Metadata struct {
 	Label Label `json:"label,omitempty"`
 	Value Label `json:"value,omitempty"`
 }
+
+func NewMetadata(label string, value string) Metadata {
+	return Metadata{
+		Label: Label{"en": {label}},
+		Value: Label{"none": {value}},
+	}
+}
